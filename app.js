8aa0762971b1633a6967c8407d90bbe1d4e7cb0c
Luke Romberg <Lukerromberg@gmail.com>
2017-10-18 20:19:42 -0600

    added queries

--- app.js
+++ app.js
@@ -1,49 +1,74 @@
+//setting the module express as a const
 const express = require('express');
+//setting the middleware body-parser as a const
 const bodyParser = require('body-parser');
+//adding a pointer to the file query.js in the db file
 const db = require('./db/query');
-
+//adding a flexible const that defines our port as either 3000 or our env port
 const port = process.env.PORT || 3000;
+//calling the express app and referring to it as app
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+//telling express to use body parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
+//route for a post on '/api/users'
 app.post('/api/users', (req, res, next) => {
-	db.createUser(req.body)
-		.then(user => res.status(201).json(user))
-		.catch(err => res.status(500).send(err));
+  //envokes the function createUser from the db page (./db/query.js)
+  db.createUser(req.body)
+    //on a successful run through, sends a 201 and user data as a json
+    .then(user => res.status(201).json(user))
+    //on a failed run through, returns a 500 and the error message
+    .catch(err => res.status(500).send(err));
 });
 
+//route for a post on '/api/users/login'
 app.post('/api/users/login', (req, res, next) => {
-	db.login(req.body.code)
-		.then(user => {
-			if(!user) {
-				res.sendStatus(401);
-			} else {
-				res.json(user);
-			}
-		}).catch(err => {
-			res.status(500).send(err);
-		});
+  //runs the login function from the db page
+  db.login(req.body.code)
+    //on success run this .then
+    .then(user => {
+      //if the response is undefined, send a 401
+      if (!user) {
+        res.sendStatus(401);
+        //if the response is not undefined, send them user data
+      } else {
+        res.json(user);
+      }
+      //on failure, send the 500 status and error
+    }).catch(err => {
+      res.status(500).send(err);
+    });
 });
 
+//route for a get request on /api/users/ with an id
 app.get('/api/users/:id', (req, res, next) => {
-	const code = req.query.code;
-	const id = req.params.id;
-
-	db.getUserById(id)
-		.then(user => {
-			if(!user) {
-				res.sendStatus(401);
-			} else if(user.code !== code) {
-				res.sendStatus(401);
-			} else {
-				res.json(user);
-			}
-		})
-		.catch(err => res.status(500).send(err));
+  //sets two const's with the data we got sent: code and id
+  const code = req.query.code;
+  const id = req.params.id;
+  //runs the funciton getUserById from the db page
+  db.getUserById(id)
+    //on success run this .then
+    .then(user => {
+      //if the user is returned as undefined, return 401
+      if (!user) {
+        res.sendStatus(401);
+        //if the user returns but the code is incorrect, return 401
+      } else if (user.code !== code) {
+        res.sendStatus(401);
+        //if the user exists and the code is correct, send the user data as a json
+      } else {
+        res.json(user);
+      }
+    })
+    //on failed run, send 500 status and error
+    .catch(err => res.status(500).send(err));
 });
 
+//listens on the port, which will be either env.port or 3000.
 app.listen(port, () => {
-	console.log(`app listening on port: ${port}`);
+  console.log(`app listening on port: ${port}`);
 });
--- db/query.js
+++ db/query.js
@@ -1,27 +1,37 @@
+//adding a pointer to the page ./connection.js and referring to it as db
 const db = require('./connection');
 
+//function creates a user
 function createUser(user) {
-	const code = generateRandomString();
-
-	user.code = code;
-
-	return db('site-users').insert(user).returning(['id', 'code']);
+  //runs the function generateRandomString() and sets the return value as code
+  const code = generateRandomString();
+  //sets the value (code) to the object key user.code
+  user.code = code;
+  //insert user into the table site-users and return the id and code of that user
+  return db('site-users').insert(user).returning(['id', 'code']);
 }
 
+//gets a user by id
 function getUserById(id) {
-	return db('site-users').first().where('id', id);
+  //goes into the table site-users and finds the first object where the 'id' value is equal to the (id) passed to us in the function
+  return db('site-users').first().where('id', id);
 }
 
+//this function "generates a random string"
 function generateRandomString() {
-	return 'abcd';
+  //returns the "random string" 'abcd'
+  return 'abcd';
 }
 
+//this function returns code when user logs in
 function login(code) {
-	return db('site-users').select().where('code', code);
+  //selects all data from the table site-users. Then returns the objects that's 'code' key value is equal to the (code) passed to the function
+  return db('site-users').select().where('code', code);
 }
 
+//exports all the functions for use on other pages by using require
 module.exports = {
-	createUser,
-	getUserById,
-	login
+  createUser,
+  getUserById,
+  login
 };
